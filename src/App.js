4576ce5febf689a7417c2f1af405cf8980e40757
sumonsheikh <sm.sumon117@gmail.com>
2021-01-23 08:55:31 +0600

    bring date, day, month, year

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 
 import React from 'react';
 const api = {
-
+  key: "0608b08c2f685a3d3300ac788c756458",
+  base: "https://api.openweathermap.org/data/2.5/"
 }
 
 function App() {
+  const dateBuilder = (d) =>{
+    let months = [ "January", "February", " March", "April", "May", "June", " July", "August", 
+    "September", "October", "November", "December"];
+    let days      = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", " Friday", "Saturday"];
+
+    let day       = days[d.getDay()];
+    let date      = d.getDate();
+    let month  = months[d.getMonth()];
+    let year      = d.getFullYear();
+    return `${day} ${date} ${month} ${year}`;
+  }
   return (
     <div className="app ">
         <main>
@@ -14,6 +26,10 @@ function App() {
                 className="search-field"
                 placeholder="serche..."
                 />
+            </div>
+            <div className="location-box">
+              <div className="location">Dhaka, Bangladesh</div>
+              <div className="date">{dateBuilder(new Date())}</div>
             </div>
         </main>
     </div>
