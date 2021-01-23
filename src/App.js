43de505b1cf52d0fdef152a93eef927313820667
sumonsheikh <sm.sumon117@gmail.com>
2021-01-23 19:06:17 +0600

    write function to fetch the weather data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 
-import React from 'react';
+import React, {useState} from 'react';
 const api = {
   key: "0608b08c2f685a3d3300ac788c756458",
   base: "https://api.openweathermap.org/data/2.5/"
 }
 
 function App() {
+  const [query, setQuery] = useState(' ');
+  const [weather, setWeather] = useState({});
+  const search = (e)=>{
+    if(e.key === 'Enter'){
+      fetch(`${api.base}weather?q=${weather}&units=metric&APPID=${api.key}`)
+      .then((res)=>res.json())
+      .then((result) =>{
+        setWeather(result)
+        setQuery(' ')
+      });
+    }
+  } 
   const dateBuilder = (d) =>{
     let months = [ "January", "February", " March", "April", "May", "June", " July", "August", 
     "September", "October", "November", "December"];
@@ -25,6 +37,9 @@ function App() {
                 type="text"
                 className="search-field"
                 placeholder="serche..."
+                onChange={e => setQuery(e.target.value)}
+                value={query}
+                onKeyPress={search}
                 />
             </div>
             <div className="location-box">
