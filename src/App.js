8c0076dd4654d50658f5624acd5584b5f4768175
sumonsheikh <sm.sumon117@gmail.com>
2021-01-23 19:41:04 +0600

    change background for temparature change

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
     return `${day} ${date} ${month} ${year}`;
   }
   return (
-    <div className="app ">
+    <div className={(typeof weather.main != "undefined")?((weather.main.temp >16)? 'app warm' : 'app'): 'app'}>
         <main>
             <div className=" search-box">
               <input
@@ -51,8 +51,8 @@ function App() {
                     <div className="date">{dateBuilder(new Date())}</div>
                  </div>
                 <div className="weather-box">
-                  <div className="temp">15°c</div>
-                  <div className="weather">Sunny</div>
+                  <div className="temp">{Math.round(weather.main.temp)}°c</div>
+                  <div className="weather">{weather.weather[0].main}</div>
                 </div>
               </div>
             ): (' ')}
